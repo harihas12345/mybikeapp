f3ef5bf054ff0da9222b71ed9e65d860cb59fe89
Aneesh Hariharan <haaneesh12345@gmail.com>
2025-12-11 10:09:24 -0800

    Fix asset paths for production deployment

--- bike-app/vite.config.js
+++ bike-app/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 8081,
     strictPort: true
   },
-  base: '/absproxy/8081/',
+  base: mode === 'development' ? '/absproxy/8081/' : '/',
   test: {
     environment: "jsdom",
   },
-})
+}))
